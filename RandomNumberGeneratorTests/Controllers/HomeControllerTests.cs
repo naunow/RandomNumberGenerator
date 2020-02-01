0d57b631a64993d7773273529be54255a0fb4d0a
naunow <naunow00@gmail.com>
2020-02-01 22:02:56 +0900

    テスト作成

---
+++ RandomNumberGeneratorTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomNumberGenerator.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RandomNumberGenerator.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void PrivacyTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace RandomNumberGeneratorTests.Controllers
+{
+    class HomeControllerTests
+    {
+    }
+}
